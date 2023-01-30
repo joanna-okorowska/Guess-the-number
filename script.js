1f42e1ebbad521f20e04d2184760a236884a1623
Joanna Okorowska <asia.okorowska@gmail.com>
2023-01-30 21:34:51 +0100

    add score and highscore

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const secretNumber = Math.trunc(Math.random() * 30) + 1;
-document.querySelector(".number").textContent = secretNumber;
-let initialScore = 30;
+let secretNumber = Math.trunc(Math.random() * 30) + 1;
+// document.querySelector(".number").textContent = secretNumber;
+let score = 30;
+let highscore = 0;
 document.querySelector(".check").addEventListener("click", () => {
-  const guess = document.querySelector(".guessInput").value;
+  const guess = Number(document.querySelector(".guessInput").value);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "You guessed the number!";
+    document.querySelector(".number").textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscoreNumber").textContent = highscore;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "The number is too high";
-    initialScore--;
-    document.querySelector(".scoreNumber").textContent = initialScore;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "The number is too high";
+      score--;
+      document.querySelector(".scoreNumber").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".scoreNumber").textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "The number is too low";
-    initialScore--;
-    document.querySelector(".scoreNumber").textContent = initialScore;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "The number is too low";
+      score--;
+      document.querySelector(".scoreNumber").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".scoreNumber").textContent = 0;
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  secretNumber = Math.trunc(Math.random() * 30) + 1;
+  score = 30;
+  document.querySelector(".scoreNumber").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guessInput").value = "";
+  document.querySelector(".message").textContent = "Your guess";
 });
